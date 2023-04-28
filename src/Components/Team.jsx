739ef1699057c5b3378aa13dcb68b4262af132e9
Nicolas Olmos <nicolas.nos@hotmail.com>
2023-04-28 14:58:08 -0500

    portal de miembros al 90%

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export const Team = () => {
   return (
-    <div>Team</div>
-  )
-}
+    <section className="team">
+      <div className="title">
+        <img src="" alt="estrella1" />
+        <h1>IA Software</h1>
+        <img src="" alt="estrella2" />
+      </div>
+      <article className="members">
+        <div className="card">
+          <img src="" alt="imagen de miembro" />
+          <div className="MInformation">
+            <h4>Jesus Moran</h4>
+            <p>email@fakeemail.com</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src="" alt="imagen de miembro" />
+          <div className="MInformation">
+            <h4>Jesus Moran</h4>
+            <p>email@fakeemail.com</p>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
+};
