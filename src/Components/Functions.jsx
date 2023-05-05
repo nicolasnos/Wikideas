38a14e947b4f83c938dc2c3cdc4b59e1a4aeed81
Jesus Moran Espinoza <jame291104@gmail.com>
2023-05-05 16:55:31 -0500

    functions terminado

--- src/Components/Functions.jsx
+++ src/Components/Functions.jsx
@@ -7,8 +7,22 @@ export const Functions = () => {
       <Header_Functions/>
       <article className='container_functions'>
         <h1 className='welcome'>Funciones de <span className='span_color'>WIKIDEAS</span></h1>
-        <div>
-          <p src="./icons8-monitor.gif" alt="" />
+        <div className='functions_e'>
+          <div className="card_f">
+            <img src="./img/foco2.png" alt="" className="image_f" />
+            <h4 className='subtitle_f'>Función 1</h4>
+            <p>Agrega definiciones de temas para que la comunidad aprenda</p>
+          </div>
+          <div className="card_f">
+            <img src="./img/hand2.png" alt="" className="image_f" />
+            <h4 className='subtitle_f'>Función 2</h4>
+            <p>Edita cualquier definición que no sea asertiva y concreta</p>
+          </div>
+          <div className="card_f">
+            <img src="./img/foco2.png" alt="" className="image_f" />
+            <h4 className='subtitle_f'>Función 3</h4>
+            <p>Califica la información para felicitar a los que hacen esto posible</p>
+          </div>
         </div>
       </article>
     </section>
