6140b0e1f1947d75ced3999c76afe133831a3907
Nicolas Olmos <nicolas.nos@hotmail.com>
2023-05-18 13:52:00 -0500

    footer responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import bitId from "../../public/img/bit_idforideas.jpg";
 import IaImg from "../../public/img/IA-Software.png";
+import Bombilla from "../../public/img/bombilla.jpg";
 
 export const Footer = () => {
   return (
     <footer>
       <article className="info">
         <div>
-          <img src="" alt="imagen de bombillo" />
+          <img src={Bombilla} alt="imagen de bombillo" />
           <p>
             Consejo: ¡Comparte esta pàgina con tus amigos y adquieran
             conociemiento juntos!
           </p>
         </div>
-        <article>
+        <article className="enterprise">
           <h2>Empresas</h2>
           <p>
             Estas empresas han hecho realidad este proyecto para nuestra
